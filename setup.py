7375e76c4219026558bef42d2e17df0f22a52064
Amber Trask <amberleightrask@gmail.com>
2017-07-23 16:21:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name = "sonar",
     version = "0.1.0",
     author = "Amber Trask",
-    author_email = "contact@openmined.com",
+    author_email = "contact@openmined.org",
     description = ("A server library (inc. Smart Contracts) for Federated Deep Learning over the Ethereum Blockchain"),
     license = "Apache-2.0",
     keywords = "deep learning machine artificial intelligence homomorphic encryption",
