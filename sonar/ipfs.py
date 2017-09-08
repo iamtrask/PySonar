7b7aa171848946d6c94756d13417a6ce6c9e9b45
Axel Hodler <axel@hodler.co>
2017-09-08 22:32:00 +0200

    adapt notebook

--- sonar/ipfs.py
+++ sonar/ipfs.py
@@ -2,7 +2,7 @@ import ipfsapi
 
 
 class IPFS:
-    def __init__(self, host, port):
+    def __init__(self, host='127.0.0.1', port=5001):
         self.ipfs = ipfsapi.connect(host, int(port))
 
     def store(self, obj):
--- sonar/test_ipfs.py
+++ sonar/test_ipfs.py
@@ -2,7 +2,7 @@ from sonar.ipfs import IPFS
 
 
 def test_retrieval_of_stored_obj():
-    storage = IPFS('127.0.0.1', 5001)
+    storage = IPFS(host='127.0.0.1', port=5001)
     obj_to_store = {'foo': 'bar'}
     address = storage.store(obj_to_store)
     retrieved_obj = storage.retrieve(address)
