708bcc47256341df9610e603fb302d59a81f7eb0
Axel Hodler <axel@hodler.co>
2017-09-23 19:50:26 +0200

    avoid autoconnection during init
    else the connection would be created even when used as
    a default parameter

--- sonar/ipfs.py
+++ sonar/ipfs.py
@@ -3,10 +3,14 @@ import ipfsapi
 
 class IPFS:
     def __init__(self, host='127.0.0.1', port=5001):
-        self.ipfs = ipfsapi.connect(host, int(port))
+        self.host = host
+        self.port = int(port)
+
+    def connect(self):
+        return ipfsapi.connect(self.host, self.port)
 
     def store(self, obj):
-        return self.ipfs.add_pyobj(obj)
+        return self.connect().add_pyobj(obj)
 
-    def retrieve(self, hash):
-        return self.ipfs.get_pyobj(hash)
+    def retrieve(self, ipfs_hash):
+        return self.connect().get_pyobj(ipfs_hash)
