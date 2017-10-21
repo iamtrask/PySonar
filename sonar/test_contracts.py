a1374d3a3c5e250b0a8b431ce93fa9609f295cc4
Andrew Trask <atrask@atrask-macbookpro.roam.corp.google.com>
2017-10-21 19:59:57 +0100

    removed test that may no longer be relevant

--- sonar/test_contracts.py
+++ sonar/test_contracts.py
@@ -1,12 +1,6 @@
 from sonar.contracts import IPFSAddress
 
 
-def test_transform_ipfs_hash_to_its_ethereum_representation():
-    sample_ipfs_hash = "QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG"
-    assert IPFSAddress().to_ethereum(sample_ipfs_hash) \
-        == ["QmYwAPJzv5CZsnA625s3Xf2nemtYgPpH", "dWEz79ojWnPbdG"]
-
-
 def test_transform_ethereum_representation_of_ipfshash_back_to_ipfshash():
     sample_ethereum_representation = \
         ["0x516d576d796f4d6f63746662416169457332473436677065556d687146524457",
