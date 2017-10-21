5a43cc9a8512c461eb39d33de5550ca978cd11a2
Andrew Trask <atrask@atrask-macbookpro.roam.corp.google.com>
2017-10-21 20:04:16 +0100

    removed tests

--- sonar/test_contracts.py
+++
@@ -1,9 +0,0 @@
-from sonar.contracts import IPFSAddress
-
-
-def test_transform_ethereum_representation_of_ipfshash_back_to_ipfshash():
-    sample_ethereum_representation = \
-        ["0x516d576d796f4d6f63746662416169457332473436677065556d687146524457",
-         "0x364b576f3634793572353831567a303030303030303030303030303030303030"]
-    assert IPFSAddress().from_ethereum(sample_ethereum_representation) \
-        == "QmWmyoMoctfbAaiEs2G46gpeUmhqFRDW6KWo64y5r581Vz"
